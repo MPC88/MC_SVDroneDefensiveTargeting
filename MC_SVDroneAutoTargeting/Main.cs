6057204909d74f8a38a2cfb834e40f049462f17f
MPC88 <myemail@gmail.com>
2023-04-14 15:15:16 +0100

    Add project files.

---
+++ MC_SVDroneAutoTargeting/Main.cs
@@ -0,0 +1,66 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace MC_SVDroneDefensiveTargeting
+{
+	[BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+	public class Main : BaseUnityPlugin
+	{
+		public const string pluginGuid = "mc.starvalor.dronedefensivetargeting";
+		public const string pluginName = "SV Drone Defensive Targeting";
+		public const string pluginVersion = "1.0.0";
+
+		public void Awake()
+		{
+			Harmony.CreateAndPatchAll(typeof(Main));
+		}
+
+        [HarmonyPatch(typeof(Drone), "FindNewTarget")]
+        [HarmonyPostfix]
+		private static void Drone_FNTPost(Drone __instance, int mode, ref Entity ___targetEntity)
+        {
+			if (__instance.owner == null)
+				return;
+
+			if (!__instance.owner.gameObject.CompareTag("Player"))
+				return;
+
+			if (__instance.ownerSS == null)
+				__instance.ownerSS = __instance.owner.GetComponent<SpaceShip>();
+
+			Collider[] array = Physics.OverlapSphere(__instance.owner.position, 200f, 512);
+			Transform x = null;
+			float num = 9999f;
+			for (int i = 0; i < array.Length; i++)
+			{
+				Transform transform = array[i].transform;
+				SpaceShip spaceShip;
+				if (transform.CompareTag("Collider"))
+					spaceShip = (transform.GetComponent<ColliderControl>().ownerEntity as SpaceShip);
+				else
+					spaceShip = transform.GetComponent<SpaceShip>();
+
+				transform = spaceShip.transform;
+				if ((mode == 1 && spaceShip != __instance.ownerSS && __instance.ownerSS.ffSys.TargetIsEnemy(spaceShip.ffSys) && !spaceShip.IsCloaked))
+				{
+					float num2 = Vector3.Distance(__instance.gameObject.transform.position, transform.position);
+					if (num2 < num)
+					{
+						num = num2;
+						x = transform;
+					}
+				}
+			}
+
+			if (x != null)
+			{
+				__instance.target = x;
+				___targetEntity = __instance.target.GetComponent<Entity>();
+				AccessTools.Method(typeof(Drone), "GetDesiredDistance").Invoke(__instance, null);
+			}
+		}
+	}
+}
