b2cd6147ed9d368a1c4a13a61f76233d6ce72c15
MPC88 <myemail@gmail.com>
2023-05-14 21:04:07 +0100

    Added null checks on several objects to fix(?) drones derping.

--- MC_SVDroneAutoTargeting/Main.cs
+++ MC_SVDroneAutoTargeting/Main.cs
@@ -2,6 +2,7 @@
 using HarmonyLib;
 using UnityEngine;
 using System.Linq;
+using BepInEx.Logging;
 
 namespace MC_SVDroneDefensiveTargeting
 {
@@ -10,8 +11,9 @@ namespace MC_SVDroneDefensiveTargeting
 	{
 		public const string pluginGuid = "mc.starvalor.dronedefensivetargeting";
 		public const string pluginName = "SV Drone Defensive Targeting";
-		public const string pluginVersion = "1.0.1";
+		public const string pluginVersion = "1.0.2";
 
+		private static ManualLogSource log = BepInEx.Logging.Logger.CreateLogSource(pluginName);
 		public void Awake()
 		{
 			Harmony.CreateAndPatchAll(typeof(Main));
@@ -21,36 +23,56 @@ namespace MC_SVDroneDefensiveTargeting
         [HarmonyPostfix]
 		private static void Drone_FNTPost(Drone __instance, int mode, ref Entity ___targetEntity)
         {
-			if (__instance.owner == null)
-				return;
-
-			if (!__instance.owner.gameObject.CompareTag("Player"))
+			if (__instance == null ||
+				__instance.owner == null ||
+				!__instance.owner.gameObject.CompareTag("Player"))
 				return;
 
 			if (__instance.ownerSS == null)
 				__instance.ownerSS = __instance.owner.GetComponent<SpaceShip>();
+			if (__instance.ownerSS == null)
+				return;
 
-            Collider[] array = Physics.OverlapSphere(__instance.owner.position, 200f, 512);
-			array = array.OrderBy(c => (__instance.owner.position - c.transform.position).sqrMagnitude).ToArray();
+			Vector3 ownerPos = __instance.owner.position;
+			Collider[] array = Physics.OverlapSphere(ownerPos, 200f, 512);
 			Transform x = null;
-			float num = 9999f;
-			for (int i = 0; i < array.Length; i++)
+			if (array.Length > 0)
 			{
-				Transform transform = array[i].transform;
-				SpaceShip spaceShip;
-				if (transform.CompareTag("Collider"))
-					spaceShip = (transform.GetComponent<ColliderControl>().ownerEntity as SpaceShip);
-				else
-					spaceShip = transform.GetComponent<SpaceShip>();
-
-				transform = spaceShip.transform;
-				if ((mode == 1 && spaceShip != __instance.ownerSS && __instance.ownerSS.ffSys.TargetIsEnemy(spaceShip.ffSys) && !spaceShip.IsCloaked))
+				array = array.OrderBy(c => (ownerPos - c.transform.position).sqrMagnitude).ToArray();				
+				float num = 9999f;
+				for (int i = 0; i < array.Length; i++)
 				{
-					float num2 = Vector3.Distance(__instance.gameObject.transform.position, transform.position);
-					if (num2 < num)
+					Transform transform = array[i].transform;
+
+					SpaceShip spaceShip = null;
+					if (transform != null)
 					{
-						num = num2;
-						x = transform;
+						if (transform.CompareTag("Collider"))
+						{
+							ColliderControl cc = transform.GetComponent<ColliderControl>();
+							if (cc != null)
+								spaceShip = (cc.ownerEntity as SpaceShip);
+						}
+						else
+							spaceShip = transform.GetComponent<SpaceShip>();
+					}
+
+					if (spaceShip != null)
+					{
+						transform = spaceShip.transform;
+
+						if (transform != null)
+						{
+							if ((mode == 1 && spaceShip != __instance.ownerSS && __instance.ownerSS.ffSys != null && __instance.ownerSS.ffSys.TargetIsEnemy(spaceShip.ffSys) && !spaceShip.IsCloaked))
+							{
+								float num2 = Vector3.Distance(__instance.gameObject.transform.position, transform.position);
+								if (num2 < num)
+								{
+									num = num2;
+									x = transform;
+								}
+							}
+						}
 					}
 				}
 			}
@@ -60,6 +82,11 @@ namespace MC_SVDroneDefensiveTargeting
 				__instance.target = x;
 				___targetEntity = __instance.target.GetComponent<Entity>();
 				AccessTools.Method(typeof(Drone), "GetDesiredDistance").Invoke(__instance, null);
+			}
+			else
+            {
+				__instance.target = null;
+				___targetEntity = null;
 			}
 		}
 	}
